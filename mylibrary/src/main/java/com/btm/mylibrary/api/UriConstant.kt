c3a0e57c7341b98b184f670b9ee348a0a80a5a68
wangbo <wangbo@id.link>
2020-07-18 00:30:26 +0800

    1.加入框架

---
+++ mylibrary/src/main/java/com/btm/mylibrary/api/UriConstant.kt
@@ -0,0 +1,12 @@
+package com.hazz.kotlinmvp.api
+
+/**
+ * Created by wb on 2018/11/16.
+ */
+object UrlConstant{
+//    const val BASE_URL = "http://192.168.1.8:6060/"
+//    const val BASE_URL = "http://127.0.0.1:8080/"
+    const val BASE_URL = "http://192.168.1.25:6060/"
+//    const val BASE_URL = "http://47.112.107.122:8080/"
+
+}
