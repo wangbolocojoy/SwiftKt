73237dce2809b88f834f2435767afa97b850fb74
wangbo <wangbo@id.link>
2020-07-18 02:04:16 +0800

    1.加入登录页面

---
+++ app/src/main/java/com/btm/swiftkt/ui/ac/LoginViewActivity.kt
@@ -0,0 +1,31 @@
+package com.btm.swiftkt.ui.ac
+
+import BaseActivity
+
+import com.btm.swiftkt.R
+
+class LoginViewActivity : BaseActivity() {
+
+    /**
+     *  加载布局
+     */
+    override fun layoutId() = R.layout.activity_login_view
+
+    /**
+     * 初始化数据
+     */
+    override fun initData() {
+    }
+
+    /**
+     * 初始化 View
+     */
+    override fun initView() {
+    }
+
+    /**
+     * 开始请求
+     */
+    override fun start() {
+    }
+}
